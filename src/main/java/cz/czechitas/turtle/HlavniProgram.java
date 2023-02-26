00e86b667caffa37356792594497b2450f588b48
Valentina <valentinanekrasova@seznam.cz>
2023-02-26 21:29:50 +0100

    Zakladni tvary

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -6,11 +6,54 @@ public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
     public void start() {
+        nakresliRvnstTroj(40); // nakresli rovnostranny trojuhelnik
+        zofka.setLocation(zofka.getX()+100, zofka.getY()+100);
 
+        nakresliRvnstTroj(100); // nakresli rovnostranny trojuhelnik
+
+        zofka.setLocation(zofka.getX()+50, zofka.getY()-100);
+
+        nakresliCtverecObdelnik(50, 50); // nakresli ctverec
+
+        zofka.setLocation(zofka.getX()-100, zofka.getY()-50);
+
+        nakresliCtverecObdelnik(100, 50); // nakresli obdelnik
+
+        zofka.setLocation(zofka.getX()-150, zofka.getY()-50);
+
+        nakresliKruh(50);
+
+    }
+
+    private void nakresliRvnstTroj ( int delkaStrany ) {/* Rovnostranny trojuhelnik */
+        for ( int i=0;i<3;i++) {
+            zofka.move(delkaStrany);
+            zofka.turnRight(120);
+        }
+    }
+
+    private void nakresliCtverecObdelnik ( int delkaStranyA, int delkaStranyB){ /* Ctverec */
+        for ( int i=0;i<2;i++){
+            zofka.move(delkaStranyA);
+            zofka.turnRight(90);
+            zofka.move(delkaStranyB);
+            zofka.turnRight(90);
+        }
+    }
+    
+    private void nakresliKruh ( int krok){
+        for (int i = 0; i < 18; i++) {
+            zofka.move(krok);
+            zofka.turnLeft(20.0);
+            
+        }
     }
 
     public static void main(String[] args) {
         new HlavniProgram().start();
     }
 
+
+
 }
+
