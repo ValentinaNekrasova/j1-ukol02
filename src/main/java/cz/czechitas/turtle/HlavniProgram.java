5f69907095b3c83b92b6e6353b096bebdeb61ffa
Valentina <valentinanekrasova@seznam.cz>
2023-02-27 12:57:52 +0100

    opraveny Domaci ukol

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -2,6 +2,10 @@ package cz.czechitas.turtle;
 
 import cz.czechitas.turtle.engine.*;
 
+import java.awt.*;
+
+import static java.awt.Color.*;
+
 public class HlavniProgram {
     private Turtle zofka = new Turtle();
 
@@ -25,40 +29,60 @@ public class HlavniProgram {
 
         //ukol03-zmrzlina
 
-        /*zofka.turnLeft(150);
-        nakresliRvnstTroj(150);
-        zofka.turnRight(180);
-        nakresliKruh(33);*/
+
+        zofka.setLocation(180,160);
+
+        nakresliZmrzlinu();
 
         //ukol03-snehulak
 
-        /*zofka.setLocation (zofka.getX()+0,zofka.getY()+100);
-        zofka.turnLeft(90);
-        nakresliKruh(28);
-        zofka.setLocation(zofka.getX()-8, zofka.getY()-115);
-        nakresliKruh(20);
-        zofka.setLocation(zofka.getX()-10, zofka.getY()-85);
-        nakresliKruh(15);
-        zofka.setLocation(zofka.getX()+80, zofka.getY()+130);
-        nakresliKruh(8);
-        zofka.setLocation(zofka.getX()-155, zofka.getY()-0);
-        nakresliKruh(8);*/
+        zofka.setLocation(zofka.getX()+200, zofka.getY()+220);
+
+        nakresliSnehulaka();
 
         //ukol03-lokomotiva
 
-        /*nakresliCtverecObdelnik(100, 200);
+        zofka.setLocation(700,400);
+
+        zofka.turnRight(60);
+
+       nakresliLokomotivu();
+    }
+
+    private void nakresliLokomotivu() {
+        nakresliCtverecObdelnik(100, 200);
         zofka.turnLeft(60);
         nakresliRvnstTroj(100);
         zofka.turnLeft(120);
-        nakresliKruh(10);
+        nakresliKruh(10, green);
         zofka.setLocation(zofka.getX()+145, zofka.getY()+0);
-        nakresliKruh(10);
+        nakresliKruh(10,green);
         zofka.setLocation(zofka.getX()+60, zofka.getY()+0);
         zofka.turnLeft(180);
         nakresliCtverecObdelnik(200,100);
         zofka.setLocation(zofka.getX()+0, zofka.getY()-22);
         zofka.turnRight(180);
-        nakresliKruh(18);*/
+        nakresliKruh(18, green );
+    }
+
+    private void nakresliSnehulaka() {
+        zofka.turnLeft(90);
+        nakresliKruh(28, orange);
+        zofka.setLocation(zofka.getX()-8, zofka.getY()-115);
+        nakresliKruh(20, orange);
+        zofka.setLocation(zofka.getX()-10, zofka.getY()-85);
+        nakresliKruh(15, orange);
+        zofka.setLocation(zofka.getX()+70, zofka.getY()+90);
+        nakresliKruh(8, orange);
+        zofka.setLocation(zofka.getX()-160, zofka.getY()-0);
+        nakresliKruh(8, orange);
+    }
+
+    private void nakresliZmrzlinu() {
+        zofka.turnLeft(150);
+        nakresliRvnstTroj(150);
+        zofka.turnRight(180);
+        nakresliKruh(33,pink);
     }
 
     private void nakresliRvnstTroj ( double delkaStrany ) {/* Rovnostranny trojuhelnik */
@@ -77,12 +101,15 @@ public class HlavniProgram {
         }
     }
     
-    private void nakresliKruh ( double krok){
+    private void nakresliKruh (double krok, Color mojeBarva){
+        Color puvodniBarva = zofka.getPenColor();
+        zofka.setPenColor( mojeBarva );
         for (int i = 0; i < 18; i++) {
             zofka.move(krok);
             zofka.turnLeft(20.0);
             
         }
+        zofka.setPenColor(puvodniBarva);
     }
 
     public static void main(String[] args) {
